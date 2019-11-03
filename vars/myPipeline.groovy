4043d46e77620cea8032b1da9ad6a46f03e21f9a
fd_big <fdjidjili@bigapps.fr>
2019-11-03 22:50:03 +0100

    test jenkinsfile

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -7,57 +7,6 @@ def call(String pipe) {
         echo ".............my_pipeline_1 ........."
 
 
-        pipeline {
-
-            agent {
-                node {
-                    label SLAVE
-                    customWorkspace "${env.WORKSPACE}/${env.JOB_NAME}_${env.BUILD_ID}"
-
-                }
-            }
-
-            stages {
-
-
-                stage('BUILD') {
-                    steps {
-                        script{
-
-                            def name = globalVars.getName(pipe)
-                            echo "build name --->:"+name
-
-
-                        }
-
-
-                    }
-                }
-
-            }
-
-            post {
-
-                always {
-
-                    echo "------------------------------- THE END ------------------------------"
-                }
-                failure {
-                    echo 'failure:  Error when executing : thank you to consult the logs on Jenkins  '
-//                    deleteDir()
-
-                }
-                success {
-
-                    echo 'Build was a success'
-                    echo 'Delete the current directory from the workspace'
-
-                    deleteDir()
-                    echo 'Delete workspace  was a success'
-                }
-            }
-
-        }
 
     }
     else if (pipe == 'my_pipeline_2') {
@@ -66,57 +15,6 @@ def call(String pipe) {
         echo ".............my_pipeline_2 ........."
 
 
-        pipeline {
-
-            agent {
-                node {
-                    label SLAVE
-                    customWorkspace "${env.WORKSPACE}/${env.JOB_NAME}_${env.BUILD_ID}"
-
-                }
-            }
-
-            stages {
-
-
-                stage('BUILD') {
-                    steps {
-                        script{
-
-                            def name = globalVars.getName(pipe)
-                            echo "build name --->:"+name
-
-
-                        }
-
-
-                    }
-                }
-
-            }
-
-            post {
-
-                always {
-
-                    echo "------------------------------- THE END ------------------------------"
-                }
-                failure {
-                    echo 'failure:  Error when executing : thank you to consult the logs on Jenkins  '
-//                    deleteDir()
-
-                }
-                success {
-
-                    echo 'Build was a success'
-                    echo 'Delete the current directory from the workspace'
-
-                    deleteDir()
-                    echo 'Delete workspace  was a success'
-                }
-            }
-
-        }
 
     }
     else if (pipe == 'my_pipeline_3') {
@@ -124,58 +22,6 @@ def call(String pipe) {
         echo "The build specific for my_pipeline_3"
         echo ".............my_pipeline_3 ........."
 
-
-        pipeline {
-
-            agent {
-                node {
-                    label SLAVE
-                    customWorkspace "${env.WORKSPACE}/${env.JOB_NAME}_${env.BUILD_ID}"
-
-                }
-            }
-
-            stages {
-
-
-                stage('BUILD') {
-                    steps {
-                        script{
-
-                            def name = globalVars.getName(pipe)
-                            echo "build name --->:"+name
-
-
-                        }
-
-
-                    }
-                }
-
-            }
-
-            post {
-
-                always {
-
-                    echo "------------------------------- THE END ------------------------------"
-                }
-                failure {
-                    echo 'failure:  Error when executing : thank you to consult the logs on Jenkins  '
-//                    deleteDir()
-
-                }
-                success {
-
-                    echo 'Build was a success'
-                    echo 'Delete the current directory from the workspace'
-
-                    deleteDir()
-                    echo 'Delete workspace  was a success'
-                }
-            }
-
-        }
 
     }
     else {
